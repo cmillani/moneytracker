7fe46dbab37d1d47b6b74aeb7b5a45dbd67653ab
Carlos Eduardo Millani <carloseduardomillani@gmail.com>
2020-01-20 14:53:17 -0300

    CEI Crawler and Backend templates generated

---
+++ MoneyTracker/backend/api/src/main/java/br/com/cadumillani/moneytracker/MoneytrackerApplication.java
@@ -0,0 +1,13 @@
+package br.com.cadumillani.moneytracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneytrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneytrackerApplication.class, args);
+	}
+
+}
---
+++ MoneyTracker/backend/api/src/test/java/br/com/cadumillani/moneytracker/MoneytrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cadumillani.moneytracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneytrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

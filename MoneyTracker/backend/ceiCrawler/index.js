0210e7f1d217bb8a9b2eef337f6113da223d41be
Carlos Eduardo Millani <carloseduardomillani@gmail.com>
2020-01-21 15:37:34 -0300

    Scrapping data from cei

--- MoneyTracker/backend/ceiCrawler/index.js
+++ MoneyTracker/backend/ceiCrawler/index.js
@@ -30,21 +30,77 @@ async function retrieveSecurePassword() {
     })
 }
 
-async function retrieveCurrentInvestmentsForCredentials(credentials) {
-    const browser = await puppeteer.launch({headless: false});
+async function retrieveCurrentInvestmentsForCredentials(browser, credentials) {
     const page = await browser.newPage();
     await page.goto('https://cei.b3.com.br/CEI_Responsivo/');
     await page.type('#ctl00_ContentPlaceHolder1_txtLogin', credentials.document);
     await page.type('#ctl00_ContentPlaceHolder1_txtSenha', credentials.password);
+    let waitLogin = page.waitForNavigation();
     await page.click('#ctl00_ContentPlaceHolder1_btnLogar');
+    await waitLogin;
+    await page.goto('https://cei.b3.com.br/CEI_Responsivo/negociacao-de-ativos.aspx');
+    let optionsSelectElement = await page.waitForSelector("#ctl00_ContentPlaceHolder1_ddlAgentes");
+
+    let allOptions = await listAllOptionsFromSelect(optionsSelectElement);
+
+    let investmentsList = []
+    for(let option of allOptions) {
+        await page.select('#ctl00_ContentPlaceHolder1_ddlAgentes', option);
+        await page.click('#ctl00_ContentPlaceHolder1_btnConsultar');
+        let table = await page.waitForSelector('#ctl00_ContentPlaceHolder1_rptAgenteBolsa_ctl00_rptContaBolsa_ctl00_pnAtivosNegociados');
+        let headerRow = await table.$('thead > tr')
+        let rows = await table.$$('tbody > tr');
+        console.log(rows)
+        console.log(headerRow)
+        investmentsList.push(...(await parseTableToJSON(headerRow, rows)));
+        await page.goto('https://cei.b3.com.br/CEI_Responsivo/negociacao-de-ativos.aspx');
+    }
+
+    return investmentsList;
 }
 
-async function processInvestmentsIntoJson(htmlInvestments) {
-    console.log(htmlInvestments);
+async function parseTableToJSON(titleRow, rows) {
+    let titles = await titleRow.$$('th');
+    let elements = [];
+    for(row of rows) {
+        let newElement = {};
+        let columns = await row.$$('td');
+        for(i in titles) {
+            let title = sanitizeText(await (await titles[i].getProperty('innerHTML')).jsonValue());
+            let value = sanitizeText(await (await columns[i].getProperty('innerHTML')).jsonValue());
+            newElement[title] = value;
+        }
+        elements.push(newElement);
+    }
+
+    return elements;
 }
 
-retrieveCredentials().then( credentials => {
-    retrieveCurrentInvestmentsForCredentials(credentials);
-}).then( htmlInvestments => {
-    processInvestmentsIntoJson(htmlInvestments);
+async function listAllOptionsFromSelect(select) {
+    let options = Array.apply(null, await select.$$('option'));
+    let allOptions = [];
+    for(option of options) {
+        allOptions.push(await (await option.getProperty('value')).jsonValue());
+    }
+
+    return allOptions.slice(1);
+}
+
+function sanitizeText(text) {
+    let clearedSpaces = text.replace(/\s/g, '');
+    let clearedTags = clearedSpaces.replace(/<\s*[^>]*>/g, '');
+    return clearedTags.replace('\\n', '');
+}
+
+puppeteer.launch({headless: false}).then ( browser => {
+    retrieveCredentials().then( credentials => {
+        return retrieveCurrentInvestmentsForCredentials(browser, credentials);
+    }).then( investments => {
+        console.log(JSON.stringify(investments));
+        return browser.close();
+    }).catch ( error => {
+        console.error(error);
+        return browser.close();
+    });
 })
+
